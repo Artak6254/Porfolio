d93b7accbf58b31f2c9bb06c835b39a8c52c7175
Artak6254 <artakp117@gmail.com>
2024-04-26 11:55:14 +0400

    add_login_email

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -4,35 +4,33 @@ import GithubIcon from "../../../public/github-icon.svg";
 import LinkedinIcon from "../../../public/linkedin-icon.svg";
 import Link from "next/link";
 import Image from "next/image";
-import emailjs from "@emailjs/browser"
+import emailjs from "@emailjs/browser";
 
 const EmailSection = () => {
   const [name, setName] = useState("");
-  const [email, setEmail] = useState("artakp117@gmail.com")
-  const [message, setMessage] = useState("")
-
-
-  
+  const [email, setEmail] = useState("artakp117@gmail.com");
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     const servicesId = "service_nnzm77w";
     const templatesId = "template_9j5gxey";
     const publicKey = "KOxaNuS1TT0dFJRZV";
-    
+
     const data = {
       from_name: name,
       from_email: email,
       message: message,
-      to_name: "Web_dev"
+      to_name: "Web_dev",
     };
-  
-    emailjs.send(servicesId, templatesId, data, publicKey)
+    setLoading(true);
+    emailjs
+      .send(servicesId, templatesId, data, publicKey)
       .then((response) => {
-        console.log("Email sent successfully", response);
+        setLoading(false);
         setName("");
-        setEmail("");
         setMessage("");
       })
       .catch((error) => {
@@ -60,74 +58,100 @@ const EmailSection = () => {
           <Link href="https://github.com/Artak6254" target="_blank">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
-          <Link href="https://linkedin.com/in/artak-petrosyan-848007232" target="_blank">
+          <Link
+            href="https://linkedin.com/in/artak-petrosyan-848007232"
+            target="_blank"
+          >
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
       </div>
       <div>
-       
-          <form className="flex flex-col" onSubmit={handleSubmit}>
-            <div className="mb-6">
-              <label
-                htmlFor="email"
-                className="text-white block mb-2 text-sm font-medium"
+        <form className="flex flex-col" onSubmit={handleSubmit}>
+          <div className="mb-6">
+            <label
+              htmlFor="email"
+              className="text-white block mb-2 text-sm font-medium"
+            >
+              My gmail
+            </label>
+            <input
+              name="email"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              id="email"
+              required
+              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              placeholder="artakp117@gmail.com"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="name"
+              className="text-white block text-sm mb-2 font-medium"
+            >
+              Name
+            </label>
+            <input
+              name="name"
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              placeholder="Write Name"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              htmlFor="message"
+              className="text-white block text-sm mb-2 font-medium"
+            >
+              Message
+            </label>
+            <textarea
+              name="message"
+              id="message"
+              value={message}
+              required
+              onChange={(e) => setMessage(e.target.value)}
+              className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
+              placeholder="Let's talk about..."
+            />
+          </div>
+
+          {loading ? (
+            <button
+              type="button"
+              className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
+              disabled
+            >
+              Loading...
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="18px"
+                fill="#fff"
+                className="ml-2 inline animate-spin"
+                viewBox="0 0 24 24"
               >
-                My gmail
-              </label>
-              <input
-                name="email"
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                id="email"
-                required
-                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="artakp117@gmail.com"
-              />
-            </div>
-            <div className="mb-6">
-              <label
-                htmlFor="name"
-                className="text-white block text-sm mb-2 font-medium"
-              >
-                Name
-              </label>
-              <input
-                name="name"
-                type="text"
-                id="name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
-                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="Write Name"
-              />
-            </div>
-            <div className="mb-6">
-              <label
-                htmlFor="message"
-                className="text-white block text-sm mb-2 font-medium"
-              >
-                Message
-              </label>
-              <textarea
-                name="message"
-                id="message"
-                value={message}
-                required
-                onChange={(e) => setMessage(e.target.value)}
-                className="bg-[#18191E] border border-[#33353F] placeholder-[#9CA2A9] text-gray-100 text-sm rounded-lg block w-full p-2.5"
-                placeholder="Let's talk about..."
-              />
-            </div>
+                <path
+                  fillRule="evenodd"
+                  d="M7.03 2.757a1 1 0 0 1 1.213-.727l4 1a1 1 0 0 1 .59 1.525l-2 3a1 1 0 0 1-1.665-1.11l.755-1.132a7.003 7.003 0 0 0-2.735 11.77 1 1 0 0 1-1.376 1.453A8.978 8.978 0 0 1 3 12a9 9 0 0 1 4.874-8l-.117-.03a1 1 0 0 1-.727-1.213zm10.092 3.017a1 1 0 0 1 1.414.038A8.973 8.973 0 0 1 21 12a9 9 0 0 1-5.068 8.098 1 1 0 0 1-.707 1.864l-3.5-1a1 1 0 0 1-.557-1.517l2-3a1 1 0 0 1 1.664 1.11l-.755 1.132a7.003 7.003 0 0 0 3.006-11.5 1 1 0 0 1 .039-1.413z"
+                  data-original="#000000"
+                />
+              </svg>
+            </button>
+          ) : (
             <button
               type="submit"
               className="bg-primary-500 hover:bg-primary-600 text-white font-medium py-2.5 px-5 rounded-lg w-full"
             >
               Send Message
             </button>
-          </form>
+          )}
+        </form>
       </div>
     </section>
   );
