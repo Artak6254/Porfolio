6f64f524855bd3f5f4de96025ac08f0c7422f850
Art <152778075+Artak6254@users.noreply.github.com>
2025-11-05 12:29:37 -0800

    change name

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -22,7 +22,7 @@ const HeroSection = () => {
             <br></br>
             <TypeAnimation
               sequence={[
-                "Artak Petrosyan",
+                "Astghik Mkrtchyan",
                 1000,
                 "Web Developer",
                 1000,
@@ -48,9 +48,6 @@ const HeroSection = () => {
               target="_blank"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-               SEE MY REZUME
-              </span>
             </Link>
           </div>
         </motion.div>
